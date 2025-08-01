7bd72a8be1fde65408e718a1849c0208925b7676
chupre <denischupritsky@gmail.com>
2025-08-01 14:07:03 +0300

    fix: cart mapper fix and error handling improvement

--- src/main/java/com/byllameister/modelstore/carts/CartMapper.java
+++ src/main/java/com/byllameister/modelstore/carts/CartMapper.java
@@ -1,11 +1,13 @@
 package com.byllameister.modelstore.carts;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CartMapper {
+    @Mapping(target = "userId", source = "user.id")
     CartDto toDto(Cart cart);
 
     List<CartDto> toDtos(List<Cart> carts);
--- src/main/java/com/byllameister/modelstore/common/GlobalExceptionHandler.java
+++ src/main/java/com/byllameister/modelstore/common/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,8 +14,13 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ErrorDto> handleHttpMessageNotReadableException(){
+    public ResponseEntity<ErrorDto> handleHttpMessageNotReadableException() {
         return ResponseEntity.badRequest().body(new ErrorDto("Invalid request body"));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorDto> handleMethodArgumentTypeMismatchException() {
+        return ResponseEntity.badRequest().body(new ErrorDto("Invalid path variable type"));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
