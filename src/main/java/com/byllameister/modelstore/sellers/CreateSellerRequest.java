518ecf7c33ee2e4c31860bfb9bed64fb929b7a79
chupre <denischupritsky@gmail.com>
2025-08-23 18:36:33 +0300

    fix(backend): seller payment details validation fix

--- src/main/java/com/byllameister/modelstore/sellers/CreateSellerRequest.java
+++ src/main/java/com/byllameister/modelstore/sellers/CreateSellerRequest.java
@@ -4,6 +4,8 @@ import com.byllameister.modelstore.common.EnumValue;
 import com.byllameister.modelstore.payments.PayoutMethod;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -16,5 +18,7 @@ public class CreateSellerRequest {
     private String payoutMethod;
 
     @NotBlank(message = "payoutDestination is required and must be not blank")
+    @Size(min = 13, max = 19, message = "payoutDestination must be between 13 and 19 digits")
+    @Pattern(regexp = "\\d+", message = "payoutDestination must contain only digits")
     private String payoutDestination;
 }
--- src/main/java/com/byllameister/modelstore/sellers/UpdateSellerRequest.java
+++ src/main/java/com/byllameister/modelstore/sellers/UpdateSellerRequest.java
@@ -1,5 +1,6 @@
 package com.byllameister.modelstore.sellers;
 
+import com.byllameister.modelstore.common.EnumValue;
 import com.byllameister.modelstore.payments.PayoutMethod;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -10,6 +11,7 @@ import lombok.Data;
 @Data
 public class UpdateSellerRequest {
     @NotNull(message = "payoutMethod is required")
+    @EnumValue(enumClass = PayoutMethod.class, ignoreCase = true, message = "Payout method must be one of: YOOMONEY_WALLET, BANK_CARD")
     private PayoutMethod payoutMethod;
 
     @NotBlank(message = "payoutDestination is required and must be not blank")
